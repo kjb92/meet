de53c1fc3081ba06d0f95b575b06f5cb013ee8f0
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 18:01:32 +0200

    test: Check for event details

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,39 @@
+import { render } from '@testing-library/react';
+import Event from '../components/Event';
+import { getEvents } from '../api';
+
+
+//Describe the scope
+describe('<Event /> component', () => {
+  let EventComponent;
+  let allEvents;
+
+  beforeAll(async () => {
+    //get mock-data via getEvents from api.js
+    allEvents = await getEvents();
+  });
+
+  beforeEach(() => {
+    EventComponent = render(<Event />);
+  });
+  
+  //Test 1
+  test('renders event title', () => {
+    expect(EventComponent.queryByText(allEvents[0].summary)).toBeInTheDocument();
+  });
+
+  //Test 2
+  test('renders event start-time', () => {
+    expect(EventComponent.queryByText(allEvents[0].start.dateTime)).toBeInTheDocument();
+  });
+
+  //Test 3
+  test('renders event location', () => {
+    expect(EventComponent.queryByText(allEvents[0].location)).toBeInTheDocument();
+  });
+
+  //Test 4
+  test('renders event details button with the title "show details"', () => {
+    expect(EventComponent.queryByText('show details')).toBeInTheDocument();
+  });
+});
