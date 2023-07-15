bcb97c447f4229e803d08e1054b5f4272359f7e4
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 17:09:13 +0200

    feat: Handle tex input change

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,9 +1,24 @@
 import { useState } from'react';
 
 
-const CitySearch = () => {
+const CitySearch = ({ allLocations }) => {
+  //true / false state
   const [showSuggestions, setShowSuggestions] = useState(false);
-  
+  //query state
+  const [query, setQuery] = useState('');
+  //filtered allLocations by query
+  const [suggestions, setSuggestions] = useState([]);
+
+  //Hanlde input change
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    const filteredLocations = allLocations ? allLocations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    }) : [];
+    setQuery(value);
+    setSuggestions(filteredLocations);
+  };
+
   return (
     <div id="city-search">
       <input 
@@ -12,9 +27,18 @@ const CitySearch = () => {
         className="city" 
         placeholder="Search for a city"
         onFocus={() => setShowSuggestions(true)}
+        onChange={handleInputChange}
+        value={query}
         />
       {showSuggestions ? (
-        <ul className="suggestions"></ul>
+        <ul className="suggestions">
+          {suggestions.map((suggestion) => (
+            <li key={suggestion}>{suggestion}</li>
+          ))}
+          <li key='See all cities'>
+            <b>See all cities</b>
+          </li>
+        </ul>
       ) : null}
     </div>
   );
