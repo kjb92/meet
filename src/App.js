19c8bd47e328a8e5b733473394edb9934fdb1cdf
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 12:24:00 +0200

    test: Re-factor show upcoming events

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+//Import EventList component
+import EventList from './components/EventList';
 
 function App() {
   return (
     <div className="App">
+      <EventList />
     </div>
   );
 }
