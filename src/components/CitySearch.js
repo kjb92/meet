5a501f424faca5a949e7c58129388c5263a85fbf
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 17:23:03 +0200

    feat: Handle click event on city suggestion item

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -19,6 +19,13 @@ const CitySearch = ({ allLocations }) => {
     setSuggestions(filteredLocations);
   };
 
+  //Handle item clicked
+  const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+    setQuery(value);
+    setShowSuggestions(false); // to hide the list
+  };
+
   return (
     <div id="city-search">
       <input 
@@ -33,9 +40,9 @@ const CitySearch = ({ allLocations }) => {
       {showSuggestions ? (
         <ul className="suggestions">
           {suggestions.map((suggestion) => (
-            <li key={suggestion}>{suggestion}</li>
+            <li key={suggestion} onClick={handleItemClicked}>{suggestion}</li>
           ))}
-          <li key='See all cities'>
+          <li key='See all cities' onClick={handleItemClicked}>
             <b>See all cities</b>
           </li>
         </ul>
