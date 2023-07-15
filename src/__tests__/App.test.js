83af95e6f0349da18690eae7dd6f0225572bcbe3
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 13:31:06 +0200

    test: Re-factor by implementing beforeEach

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,16 +3,19 @@ import App from '../App';
 
 //Describe the first test scope
 describe('<App /> component', () => {
-  
+  let AppDOM;
+  //Before each test
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  });
+
   //Test 1
   test('renders list of events', () => {
-    const AppDOM = render(<App />).container.firstChild;
     expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
   });
 
   //Test 2
   test('render CitySearch', () => {
-    const AppDOM = render(<App />).container.firstChild;
     expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
   });
 });
