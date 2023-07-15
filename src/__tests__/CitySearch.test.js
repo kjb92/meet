6fe2742874aa969ff3836fc4f6dcfbb148fc06ca
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 13:59:31 +0200

    test: Check for suggestions list

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,13 +1,37 @@
 import { render } from '@testing-library/react';
-import CitySearch from './CitySearch';
+import userEvent from '@testing-library/user-event';
+import CitySearch from '../components/CitySearch';
+
 
 //Describe the scope
 describe('<CitySearch /> component', () => {  
+  let CitySearchComponent;
+  let cityTextBox;
+  let user; 
+  beforeEach(() => {
+    CitySearchComponent = render(<CitySearch />);
+    cityTextBox = CitySearchComponent.queryByRole('textbox');
+    //Setup the object that will represent the user for testing purposes
+    user = userEvent.setup();
+  });
+  
   //Test 1
   test('renders text input', () => {
-    const CitySearchComponent = render(<CitySearch />);
-    const cityTextBox = CitySearchComponent.queryByRole('textbox');
     expect(cityTextBox).toBeInTheDocument();
     expect(cityTextBox).toHaveClass('city');
+  });
+
+  //Test 2
+  test('suggestions list is hidden by default', () =>{
+    const suggestionsList = CitySearchComponent.queryByRole('list');
+    expect(suggestionsList).not.toBeInTheDocument();
+  });
+
+  //Test 3
+  test('renders a list of suggestions when city textbox gains focus', async () => {
+    await user.click(cityTextBox);
+    const suggestionsList = CitySearchComponent.queryByRole('list');
+    expect(suggestionsList).toBeInTheDocument();
+    expect(suggestionsList).toHaveClass('suggestions');
   });
 });
