b0b1a8902e2558d05dca6cade536a3eb7343969c
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 13:33:55 +0200

    test: Re-factor by implementing beforeEach

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -3,15 +3,19 @@ import EventList from '../components/EventList';
 
 //Describe the scope
 describe('<EventList /> component', () => {
+  let EventListComponent;
+  beforeEach(() => {
+    EventListComponent = render(<EventList />);
+  });
+  
   //Test 1
   test('has an element with "list" role', () => {
-    const EventListComponent = render(<EventList />);
     expect(EventListComponent.getByRole('list')).toBeInTheDocument();
   });
 
   //Test 2
   test('renders correct number of events', () => {
-    const EventListComponent = render(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}/>);
+    EventListComponent.rerender(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}/>);
     expect(EventListComponent.getAllByRole('listitem')).toHaveLength(4);
   });
 });
