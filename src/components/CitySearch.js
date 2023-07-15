4c7640928426eaabf9dd2b5d52b8928d464b585d
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 13:43:31 +0200

    test: Re-factor CitySearch

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,6 +1,13 @@
 const CitySearch = () => {
   return (
-    <div id="city-search"></div>
+    <div id="city-search">
+      <input 
+        type="text" 
+        id="city-search-input" 
+        className="city" 
+        placeholder="Search for a city"
+        />
+    </div>
   );
 };
 
