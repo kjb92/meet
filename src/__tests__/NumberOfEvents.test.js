901d8429009ad6aeb0365b04d44b225175b891a7
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 20:10:13 +0200

    test: Check for number of events input

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NumberOfEvents from '../components/NumberOfEvents';
+import { getEvents } from '../api';
+
+
+//Describe the scope
+describe('<NumberOfEvents /> component', () => {  
+  let NumberOfEventsComponent;
+  let numberTextBox;
+  beforeEach(() => {
+    NumberOfEventsComponent = render(<NumberOfEvents />);
+    numberTextBox = NumberOfEventsComponent.container.querySelector('#number-of-events-input');
+  });
+  
+  //Test 1
+  test('renders number input', () => {
+    expect(numberTextBox).toBeInTheDocument();
+    expect(numberTextBox).toHaveClass('number-of-events');
+  });
+
+});
