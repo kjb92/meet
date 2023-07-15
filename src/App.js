e5000093bc38685c1b31fe206b8b23c3e33bae17
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 19:57:25 +0200

    feat: Render NumberOfEvents component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 //Import EventList component
 import EventList from './components/EventList';
+//Import CitySearch component
 import CitySearch from './components/CitySearch';
+//Import NumberOfEvents component
+import NumberOfEvents from './components/NumberOfEvents';
 
 function App() {
   return (
     <div className="App">
       <CitySearch />
       <EventList />
+      <NumberOfEvents />
     </div>
   );
 }
