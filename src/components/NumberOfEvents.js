2a0ec6a1aade9a7aeb21ff451fbf378ef4d66b70
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 20:17:41 +0200

    feat: Change number of events input

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 const NumberOfEvents = () => {
+  const [numberOfEvents, setNumberOfEvents] = useState(32);
+
+  //Hanlde input change
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setNumberOfEvents(value);
+  };
+  
   return (
     <div id="number-of-events">
       <input 
         type="text" 
         id="number-of-events-input" 
         className="number-of-events" 
+        value={numberOfEvents}
+        onChange={handleInputChange}
         />
     </div>
   );
