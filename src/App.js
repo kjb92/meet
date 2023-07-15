9bf1a67b4371ffc7773fa1931dbce3b995519b5b
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 13:27:25 +0200

    test: List of city suggestions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 //Import EventList component
 import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
 
 function App() {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,9 +3,16 @@ import App from '../App';
 
 //Describe the first test scope
 describe('<App /> component', () => {
-  //Write the first test
+  
+  //Test 1
   test('renders list of events', () => {
     const AppDOM = render(<App />).container.firstChild;
     expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+  });
+
+  //Test 2
+  test('render CitySearch', () => {
+    const AppDOM = render(<App />).container.firstChild;
+    expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
   });
 });
