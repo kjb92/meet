6f7274f64c75e62812121164e18a5824c99336e3
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-15 19:48:08 +0200

    feat: Show / hide event details

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,13 +1,27 @@
-const Event = ( {event} ) => {
+import { useState } from "react";
+
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
   return (
     <li>
       <h2>{event.summary}</h2>
       <p>{event.start.dateTime}</p>
       <p>{event.location}</p>
-      <button>show details</button>
+      {showDetails === true ? (
+        <>
+          <button id="hide-details" onClick={toggleDetails}>hide details</button>
+          <div id="event-details"></div>
+        </>
+      ) : (
+        <button id="show-details" onClick={toggleDetails}>show details</button>
+      )}
     </li>
-  )
+  );
 };
-
 
 export default Event;
