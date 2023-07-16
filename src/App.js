cf3cd724453d8fb94c815f762e267f3d653f2353
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-16 10:33:54 +0200

    feat: Enable user to change number of events

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
-import { useState } from "react"; 
+import { useState, useEffect } from "react"; 
 //Import EventList component
 import EventList from './components/EventList';
 //Import CitySearch component
 import CitySearch from './components/CitySearch';
 //Import NumberOfEvents component
 import NumberOfEvents from './components/NumberOfEvents';
+//Import getEvents & extractLocations
+import { getEvents, extractLocations } from "./api";
+
 
 function App() {
+  const [events, setEvents] = useState([]);
+  const [locations, setLocations] = useState([]);
   const [numberOfEvents, setNumberOfEvents] = useState('32');
+
+  //Get all events function
+  async function getAllEvents() {
+    const eventList = await getEvents();
+    setEvents(eventList);
+    setLocations(extractLocations(eventList));
+  };
+
+  //useEffect: Get all events
+  useEffect(() => {
+    getAllEvents();
+  }, []);
   
   //Handle number of events change
   const handleNumberOfEventsChange = (newNumber) => {
@@ -17,12 +34,12 @@ function App() {
 
   return (
     <div className="App">
-      <CitySearch />
-      <EventList />
+      <CitySearch allLocations={locations}/>
       <NumberOfEvents 
         numberOfEvents={numberOfEvents}
         handleNumberOfEventsChange={handleNumberOfEventsChange}
       />
+      <EventList events={events.slice(0, numberOfEvents)}/>
     </div>
   );
 }
--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,7 +1,7 @@
 //Import Event component
 import Event from './Event';
 
-const EventList = ({ events = [] }) => {
+const EventList = ({ events }) => {
   return (
     <ul id="event-list">
       {events ? events.map(event => <Event key={event.id} event={event} />) : null}
