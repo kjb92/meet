62d5092053b23ba5db50a043ce43e84cbfade267
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-16 14:32:27 +0200

    test: Render suggestion list when app is rendered

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
+import App from '../App';
 import { getEvents, extractLocations } from '../api';
 
 
-//Describe the scope
+//Scope 1
 describe('<CitySearch /> component', () => {  
   let CitySearchComponent;
   let cityTextBox;
@@ -77,4 +78,23 @@ describe('<CitySearch /> component', () => {
     expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
   });
 
+});
+
+//Scope 2
+describe('<CitySearch /> integration', () => {
+  test('renders suggestions list when the app is rendered.', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const CitySearchDOM = AppDOM.querySelector('#city-search');
+    const cityTextBox = within(CitySearchDOM).queryByRole('textbox');
+    await user.click(cityTextBox);
+
+    const allEvents = await getEvents();
+    const allLocations = extractLocations(allEvents);
+
+    const suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem');
+    expect(suggestionListItems.length).toBe(allLocations.length + 1);
+ });
 });
