b31ae0388d08f03364df192acb3bab747c4cf4ef
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-16 14:23:31 +0200

    test: Check for 32 events when app is mounted

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,8 +1,9 @@
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 import EventList from '../components/EventList';
+import App from '../App';
 import { getEvents } from '../api';
 
-//Describe the scope
+//Scope 1
 describe('<EventList /> component', () => {
   let EventListComponent;
   beforeEach(() => {
@@ -19,5 +20,19 @@ describe('<EventList /> component', () => {
     const allEvents = await getEvents();
     EventListComponent.rerender(<EventList events={allEvents}/>);
     expect(EventListComponent.getAllByRole('listitem')).toHaveLength(allEvents.length);
+  });
+});
+
+//Scope 2
+describe('<EventList /> integration', () => {
+  //Test 1
+  test('renders a list of 32 events when the app is mounted and rendered', async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBe(32);
+    });
   });
 });
