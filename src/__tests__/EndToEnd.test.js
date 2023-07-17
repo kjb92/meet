41fe05e93bc441f80b122381c7c60b9df7ad2e7f
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-17 19:43:30 +0200

    test: Write e2e test for feat 2 scenario 1

---
+++ src/__tests__/EndToEnd.test.js
@@ -0,0 +1,22 @@
+import { jest } from '@jest/globals';
+import puppeteer from 'puppeteer';
+
+describe('show/hide an event details', () => {
+  //TEST: Feature 2 - Scenario 1
+  test('An event element is collapsed by default', async () => {
+    jest.setTimeout(30000); // Increase the timeout to 10 seconds
+
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+
+    // if your event element has a different selector, use it instead of .event
+    await page.waitForSelector('.event');
+
+    // if your event's details have a different selector, use it instead of .event .details
+    const eventDetails = await page.$('#event-details');
+    expect(eventDetails).toBeNull();
+    browser.close();
+  });
+});
