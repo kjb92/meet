4f10eba5fb5eab568466b8d34b8ed4ab443f1701
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-18 08:07:15 +0200

    test: Refactor e2e tests to increase performance

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -2,37 +2,33 @@ import { jest } from '@jest/globals';
 import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
   //TEST: Feature 2 - Scenario 1
   test('An event element is collapsed by default', async () => {
     jest.setTimeout(30000); // Increase the timeout to 30 seconds
 
-    const browser = await puppeteer.launch();
-
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-
-    // if your event element has a different selector, use it instead of .event
-    await page.waitForSelector('.event');
-
-    // if your event's details have a different selector, use it instead of .event .details
     const eventDetails = await page.$('#event-details');
     expect(eventDetails).toBeNull();
-    browser.close();
   });
 
     //TEST: Feature 2 - Scenario 2
     test('User can expand an event to see its details', async () => {
       jest.setTimeout(30000); // Increase the timeout to 30 seconds
 
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.goto('http://localhost:3000/');
-  
-      await page.waitForSelector('.event');
-      await page.click('.event .details-btn');
-  
+      await page.click('.event .details-btn'); //this is the show details button in the event component
       const eventDetails = await page.$('#event-details');
       expect(eventDetails).toBeDefined();
-      browser.close();
     });
 });
