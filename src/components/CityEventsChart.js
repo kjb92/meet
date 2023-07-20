681b919eabd1cddc7f80f9edb98366a004fa3d0c
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-07-20 04:55:26 +0200

    fix: XAxis responsiveness issues

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -21,7 +21,7 @@ const CityEventsChart = ({ allLocations, events }) => {
   const getData = () => {
     const data = allLocations.map((location) => {
     const count = events.filter((event) => event.location === location).length
-      const city = location.split(', ')[0]
+      const city = location.split((/, | - /))[0] //Regex to split by either comma or hyphen
       return { count, city };
     })
     return data;
@@ -33,12 +33,12 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 20,
           right: 20,
-          bottom: 20,
+          bottom: 60,
           left: 20,
         }}
       >
         <CartesianGrid />
-        <XAxis type="category" dataKey="city" name="City" />
+        <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}/>
         <YAxis type="number" dataKey="count" name="Number of Events" allowDecimals={false} />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="An event" data={data} fill="#8884d8" />
